c527d2efd52fbc6c8d6c4ef01e35087af6aa40c6
balajinikhil <balajiedward007@gmail.com>
2020-07-09 16:04:21 +0530

    Updates

---
+++ precache-manifest.2f002309e58acbe8cf6bb38a05752ef8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4280ec5ee8a44d89febab1ff42dbc0f",
+    "url": "/video-react/index.html"
+  },
+  {
+    "revision": "9c9cae46cbb15edcdf07",
+    "url": "/video-react/static/css/main.9f8519fe.chunk.css"
+  },
+  {
+    "revision": "0c72aeaeab41d0b9c54f",
+    "url": "/video-react/static/js/2.aee3077b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/video-react/static/js/2.aee3077b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c9cae46cbb15edcdf07",
+    "url": "/video-react/static/js/main.7b1a3d64.chunk.js"
+  },
+  {
+    "revision": "c45703fe72eba4f4451e",
+    "url": "/video-react/static/js/runtime-main.cbfdeac6.js"
+  }
+]);
